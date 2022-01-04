fb4d6e193be1cbff04178a02d2fb46e081101c48
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-01-04 13:53:47 +0100

    se cologa al la barra de nuevo alumnos como nodo hijo

---
+++ practica nodos colocacion/mine.js
@@ -0,0 +1,25 @@
+// let etiquetas= document.getElementsByTagName("p");
+// let etiquetas2= document.getElementsByTagName("h2");
+// console.log(etiquetas);
+// console.log(etiquetas2);
+
+// console.log(etiquetas2[5]);
+
+// let nuevoNodo = document.createElement("div");
+// nuevoNodo.innerText = "chaco"
+// (etiquetas2[5]).appendChild(nuevoNodo);
+
+
+
+// let nuevoNodo2 = document.createElement("div")
+// nuevoNodo2.innerText = 'Paris'
+// etiquetas2[4].appendChild(nuevoNodo2);
+
+comienzo = document.getElementById("comienzo");
+comienzo.innerText= "nuevo"
+
+
+// para crear
+nuevoDiv = document.createElement("button")
+nuevoDiv.innerText= "comienzo";
+comienzo.appendChild(nuevoDiv)
--- valido.js
+++ valido.js
@@ -1,48 +1,69 @@
 // INGRESO VALIDO DEL USUARIO-----------------------------------------------------------------
 function valido(){
     
-    alert(" Ingreso Valido")
+    // alert(" Ingreso Valido")
     botonIngresar.parentNode.removeChild(botonIngresar);
-    
-
     acceso.parentNode.removeChild(acceso);
+    
     const div = document.createElement("div")
     div.innerHTML = 
     "<center><h1> Bienvenido a la aplicacion</h1><button id=comenzar>COMENZAR</button></center>";
     document.body.appendChild(div);
+    
     botonComenzar = document.getElementById("comenzar");
     botonComenzar.onclick = () => 
-   
-
     tableroDeTareas();
 };  
 // TABLERO DE TAREAS PRINCIPAL-----------------------------------------------------------------
 function tableroDeTareas(){
 
     botonComenzar.parentNode.removeChild(botonComenzar);
-
-    let botonesListaTares = document.createElement("center")
-    botonesListaTares.innerHTML = "<button id=ingresarAlumnoNuevo> INGRESAR ALUMNO NUEVO</button> <br> <button>LISTA DE ALUMNOS</button><br> <button id = actividades>ACTIVIDADES</button>";
+    
+    botonesListaTares = document.createElement("div")
+    botonesListaTares.innerHTML = "<center><button id=ingresarAlumnoNuevo> INGRESAR ALUMNO NUEVO</button> <br> <button>LISTA DE ALUMNOS</button><br> <button id = actividades>ACTIVIDADES</button></center>";
     document.body.appendChild(botonesListaTares);
-
+    
     const botonAgregarAlumno = document.getElementById("ingresarAlumnoNuevo");
     const actividades = document.getElementById("actividades")
 
+    botonAgregarAlumno.onclick = () => 
+    agregar();
 
     actividades.onclick =() =>
     actividades1();
-    botonAgregarAlumno.onclick = () => 
-    agregar();
+
+
+
+
 }
 
 
 
 // //  METODO-> AGREGAR ALUMNO ------------------------------------------------------------------
 function agregar(){
+    //  botonAgregarAlumn.parentNode.removeChild(botonAgregarAlumn);
+
     tablaIngresarAlumnoNuevo = document.createElement("center");
-    tablaIngresarAlumnoNuevo.innerHTML = "<div class= conteiner><form ><input id= Nombre type=text placeholder=Nombre><input id= edad type=number placeholder=Edad><button id=boton type=reset>AGREGAR</button></form></div>";
-    document.body.appendChild(tablaIngresarAlumnoNuevo);}
+    tablaIngresarAlumnoNuevo.innerHTML = "<div id= conteiner><form ><input id= Nombre type=text placeholder=Nombre><input id= edad type=number placeholder=Edad><button id=boton type=reset>AGREGAR</button></form></div><br><button id=volver>VOLVER</button>";
+   
+//  EL TABLERO PARA AGRAGR ALUMNOS SE AGRGA COMO NODO HIJO DEL DIV PRINCIPAL DE BOTONES DE LISTA
+    botonesListaTares.appendChild(tablaIngresarAlumnoNuevo);
+
+//    casa= document.getElementById("conteiner")
+   
+    volver = document.getElementById("volver");
+    volver.onclick= ()=>
+    volverF()
+
+}
+
 
 // DEBO CREAR UN METODO QUE TOME LOS DATOS INGRESADOS Y LOS CONVIERTA EN UN ARRAY, PARA LUEGO GUARDAR ESA INFO EN EL LOCAR STORAGE CON JSON, VOLVERLO A OBJETO PARA QUE ESA INFO SEA UTILIZADA EN LISTA DE ALUMNOS
 
 
+function volverF() {
+
+    tablaIngresarAlumnoNuevo.parentNode.removeChild(tablaIngresarAlumnoNuevo);
+
+
+}
